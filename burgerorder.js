4ba1a2f0936e459d0dc7146baac068a65b8a7bde
Meikiene <42394095+Meikiene@users.noreply.github.com>
2018-09-04 12:37:00 -1000

    Add files via upload
    first commit

---
+++ burgerorder.js
@@ -0,0 +1,18 @@
+var hamburger = {
+meat: "beef",
+veg: ["Lettuce", "Tomato", "Pickles"],
+cheese: "Mozzarella",
+mayo: true,
+mustard: false,
+ketchup: true,
+bbq: false,
+price: 7,
+qty: 1
+};
+window.alert(hamburger.veg[1]);
+//Ask the user how many
+//var question = prompt("How many burgers are you buying?");
+//Tell the user how many they are buying
+//window.alert("You are buying" + " " + question + " burger");
+//Tell the user how much they owe
+//window.alert("Now you owe" + " " + hamburger.price * question);
